262838d39b129f5a78918c3006be1e94c32b5696
mayagold <maya.gold.1991@gmail.com>
2017-09-08 13:05:55 -0700

    JWT sign up and log in functionality

--- tour206_api/config/application.rb
+++ tour206_api/config/application.rb
@@ -20,7 +20,8 @@ module Tour206Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    # Time Zone
+    config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -29,5 +30,6 @@ module Tour206Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    require "dotenv-rails"
   end
 end
--- tour206_api/config/routes.rb
+++ tour206_api/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users do
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
   get 'events/index'
 
   resources :shows
