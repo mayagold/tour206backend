958ad919c63377389f8fa16088b22e361b823f18
David Gardner <dpgardner10@gmail.com>
2017-09-08 13:09:47 -0700

    saved to var

--- tour206_api/app/controllers/events_controller.rb
+++ tour206_api/app/controllers/events_controller.rb
@@ -1,10 +1,18 @@
 require 'rubygems'
 require 'httparty'
+require 'json'
 
 class EventsController < ApplicationController
   def index
     response = HTTParty.get('https://www.eventbriteapi.com/v3/events/search/?q=concert&location.address=seattle&token=ISIHLNSRVCTAIEHALGNE')
 
-render json: { data: response.body, message: response.message }
+#     .parsed_response
+# respond_to do |format|
+#   format.json {render :json => JSON.parse(response)}
+
+events = render json: { data: response.body, message: response.message }
+
+puts events
+
   end
 end
