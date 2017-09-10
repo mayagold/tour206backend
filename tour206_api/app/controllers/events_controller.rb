adaf20b3d0eb420f98108e9a1226186af5e00c26
mayagold <maya.gold.1991@gmail.com>
2017-09-09 21:39:42 -0700

    saturday nite

--- tour206_api/app/controllers/events_controller.rb
+++ tour206_api/app/controllers/events_controller.rb
@@ -1,9 +1,12 @@
 require 'rubygems'
 require 'httparty'
 
+url = 'https://www.eventbriteapi.com/v3/events/search/?q=concert&location.address=seattle&token=ISIHLNSRVCTAIEHALGNE'
+
 class EventsController < ApplicationController
   def index
-    response = HTTParty.get('https://www.eventbriteapi.com/v3/events/search/?q=concert&location.address=seattle&token=ISIHLNSRVCTAIEHALGNE')
+    response = HTTParty.get(url)
+    JSON.parse(response)
 
 render json: { data: response.body, message: response.message }
   end
