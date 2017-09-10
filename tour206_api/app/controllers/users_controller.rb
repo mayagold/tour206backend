23c58ce39e69122ae9f3ee6b9443b09a58fc54b3
mayagold <maya.gold.1991@gmail.com>
2017-09-09 19:22:50 -0700

    user reg works and user is immediately logged in upon registration

--- tour206_api/app/controllers/users_controller.rb
+++ tour206_api/app/controllers/users_controller.rb
@@ -79,6 +79,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :email, :password_digest)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
