af244de3dd07ca47d3e6f3f3a664c79b889e0f91
mayagold <maya.gold.1991@gmail.com>
2017-09-11 10:49:51 -0700

    checking out

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,8 +5,8 @@ class EventsController < ApplicationController
   def index
     response = HTTParty.get('https://www.eventbriteapi.com/v3/events/search/?location.address=seattle&categories=103&token=ISIHLNSRVCTAIEHALGNE')
 
-@events = render json: response.body
-puts @events
+    @events = render json: response.body
+    puts @events
   end
 
 end
---
+++ db/migrate/20170911174826_add_array_to_users.rb
@@ -0,0 +1,4 @@
+class AddArrayToUsers < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
