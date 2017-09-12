b0ca69766ab3e99d50bb223c914a74e2459e01db
mayagold <maya.gold.1991@gmail.com>
2017-09-12 11:48:17 -0700

    testing token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
         token = create_token(user.id, user.username)
         render json: {status: 200, token: token, user: user}
       else
-        render json: {status: 401, message: "Unauthorized"}
+        render json: {status: 401, message: "Unauthorized", token: token}
       end
     end
 
