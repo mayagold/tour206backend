f85dfd6578862188a6249f61852c3212f3da7091
mayagold <maya.gold.1991@gmail.com>
2017-09-12 09:23:07 -0700

    pushing changes to heroku

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,9 +5,9 @@ class EventsController < ApplicationController
   def index
     response = HTTParty.get('https://www.eventbriteapi.com/v3/events/search/?location.address=seattle&categories=103&token=ISIHLNSRVCTAIEHALGNE')
 
-@events = render json: response.body
-puts @events
-  end
+    @events = render json: response.body
+      puts @events
+    end
 
 
 end
